38a40b3ccec127191884709c4476dfbf50d04aa6
lelandbatey <lelandbatey@gmail.com>
2014-12-04 21:30:17 -0800

    Fixing prior rebase

--- videoConverter.sh
+++ videoConverter.sh
@@ -25,15 +25,9 @@ OUTPUT_DIR="$6"
 # 
 # 1. `youtube_video_link` the http:// link to the youtube video, to be downloaded by youtube-dl.
 # 2. `start_time` formatted as "HH:MM:SS"
-<<<<<<< HEAD
-# 3. `end_time` formatted as "SS"
-# 4. `gif_name` (name of gif)
-# 5. `gif_width` width of the gif, in pixels
-=======
 # 3. `duration` formatted as "SS"
 # 4. `gif_name` the name of the final gif
 # 5. `width` as number of pixels
->>>>>>> Modified gifmachine to create webm files.
 # 6. `output_dir` as a path to a directory
  
 
@@ -53,47 +47,19 @@ else
 	fi
 fi
 
-# Done verifying arguments
-
 # Downloads the video file:
-# youtube-dl -f 34/35/18/6 -o "video.mp4" $1
-
 youtube-dl --no-playlist -f 18/17/22 -o "video.mp4" $1
 
 # Layout of how the FFMPEG command needs to look:
 #     ffmpeg -i the_video_file.mp4 -ss {start_timestamp} -t {duration} out%04d.png
-<<<<<<< HEAD
-echo "avconv -i video.mp4 -ss $START_TIME -t $END_TIME out%04d.png"
-avconv -loglevel panic -i video.mp4 -ss "$START_TIME" -t "$END_TIME" out%04d.png
-
-
-# Combines all the frames into one very nicely animated gif.
-convert -layers optimize -fuzz 1.5% -delay 4 out*.png -resize "$GIF_WIDTH" anim.gif 
-
-
-# If the user hasn't passed in an output directory, then set the output
-# directory to be the original directory. This is done because this script may
-# be used outside of automated environments.
-if [ -z "$OUTPUT_DIR" ]; then
-	OUTPUT_DIR="$ORIGINAL_DIR"
-fi
-
-
-# Moving our completed gif back to our correct directory, with the correct
-# name (if specified).
-if [ -n "$GIF_NAME" ]; then
-	mv anim.gif $ORIGINAL_DIR$GIF_NAME
-else
-	mv anim.gif $ORIGINAL_DIR
-fi
-=======
 echo "ffmpeg -i video.mp4 -ss $2 -t $3 out%04d.png"
 ffmpeg -loglevel panic -i video.mp4 -ss "$2" -t "$3" out%04d.png
+
+# Convert to webm video
 ffmpeg -i out%04d.png -c:v libvpx -b:v 1M -crf 4 anim.webm
 
 OUTPUT_DIR="$6"
 mv anim.webm $OUTPUT_DIR$4
->>>>>>> Modified gifmachine to create webm files.
 
 
 # Cleaning up
